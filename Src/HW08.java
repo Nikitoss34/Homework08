ac1bb490764c4ce30c5ec43563ff07335ae09e3b
Mykyta <honcharov.nikita70@gmail.com>
2023-08-12 19:44:19 +0200

    first commit

--- Src/HW08.java
+++ Src/HW08.java
@@ -3,10 +3,18 @@ import java.util.Random;
 public class HW08 {
     public static void main(String[] args) {
         Random random = new Random();
-
-
+        int pcNum = random.nextInt(10);
+        int tries = 1;
+        for (; tries <= 3; tries++) {
+            int automaticNum = random.nextInt(10);
+            if (automaticNum == pcNum) {
+                System.out.println("Ми вгадали число =" + pcNum + " з спроби № " + tries);
+                break;
+            }
         }
-
+         if (tries >= 4) System.out.println("Ми не вгадали число = " + pcNum);
 
     }
+
+
 }
